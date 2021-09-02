8a474023b5d1fe047fc9ff3d3d6d8633d47d5847
yuriqpaiva <yuriqpaiva@gmail.com>
2021-09-02 10:28:10 -0300

    update img component

--- src/components/AvatarImage.tsx
+++ src/components/AvatarImage.tsx
@@ -1,10 +1,6 @@
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
-
-
 
 export default function AvatarImage() {
-
     return (
         <div className={`
             m-5 
@@ -12,7 +8,8 @@ export default function AvatarImage() {
             <Image src={'https://avatars.githubusercontent.com/u/77457083?v=4'}
                 width={200} height={200}
                 className="rounded-full "
-                alt=''
+                alt='Profile Image'
+                loading='lazy'
             />
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'fade-in-down': {
           '0%': {
             opacity: '0',
-            transform: 'translateY(-10px)'
+            transform: 'translateY(-50px)'
           },
           '100%': {
             opacity: '1',
